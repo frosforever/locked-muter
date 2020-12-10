f563b1633ada5b6f5f0eea69e234cf6fbd58ac52
frosforever <frosforever@users.noreply.github.com>
2020-12-10 10:56:48 -0500

    Add listeners for screen lock and unlock

--- locked-muter/main.swift
+++ locked-muter/main.swift
@@ -6,6 +6,39 @@
 //
 
 import Foundation
+import CoreAudioKit
 
-print("Hello, World!")
+NSLog("Starting locked-muter")
 
+let notifcationCenter = DistributedNotificationCenter.default()
+
+// Used to track whether we have done the mutation
+var mutedTheVolume: Bool = false
+
+notifcationCenter.addObserver(
+    forName: .init("com.apple.screenIsLocked"),
+    object: nil,
+    queue: .main,
+    using: { not in
+        if (!NSSound.systemVolumeIsMuted) {
+            NSSound.systemVolumeIsMuted = true
+            mutedTheVolume = true
+            NSLog("Muted")
+        }
+    }
+)
+
+notifcationCenter.addObserver(
+    forName: .init("com.apple.screenIsUnlocked"),
+    object: nil,
+    queue: .main,
+    using: { not in
+        if (mutedTheVolume && NSSound.systemVolumeIsMuted) {
+            NSSound.systemVolumeIsMuted = false
+            mutedTheVolume = false
+            NSLog("UnMuted")
+        }
+    }
+)
+
+CFRunLoopRun()
